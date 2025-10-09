a89c408ffd1111f0b90723cd9e034fa8c97545a5
matthiashimmelmann <matthias.himmelmann@outlook.de>
2025-10-09 10:26:01 +0200

    Changed make file

--- docs/make.jl
+++ docs/make.jl
@@ -1,26 +1,50 @@
 using Pkg
-Pkg.develop(PackageSpec(path=pwd()));
+is_ci = get(ENV, "GITHUB_ACTIONS", "false") == "true"
+if is_ci
+    Pkg.develop(PackageSpec(path=pwd()));
+end
 Pkg.instantiate()
 using Documenter
 include("../src/DeformationPaths.jl")
 
-makedocs(
-    sitename = "DeformationPaths.jl",
-    pages = [
-        "Home" => "index.md",
-        "Usage Guide" => "usage.md",
-        "API Reference" => [
-            "DeformationPath" => "DeformationPath.md", 
-            "ConstraintSystem" => "ConstraintSystems.md", 
-            "Visualization" => "Visualization.md",
-            "Auxiliary Methods" => "Auxiliary.md",
-        ]    
-    ],
-    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true", sidebar_sitename = true, assets = ["assets/custom.css"]),
-    authors = "Matthias Himmelmann",
-    modules = [DeformationPaths.DeformationPaths]
-)
-
+if is_ci
+    @info "Running in GitHub Actions CI"
+    makedocs(
+        sitename = "DeformationPaths.jl",
+        pages = [
+            "Home" => "index.md",
+            "Usage Guide" => "usage.md",
+            "API Reference" => [
+                "DeformationPath" => "DeformationPath.md", 
+                "ConstraintSystem" => "ConstraintSystems.md", 
+                "Visualization" => "Visualization.md",
+                "Auxiliary Methods" => "Auxiliary.md",
+            ]    
+        ],
+        format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true", sidebar_sitename = true, assets = ["assets/custom.css"]),
+        authors = "Matthias Himmelmann",
+        modules = [DeformationPaths.DeformationPaths]
+    )
+else
+    @info "Running locally"
+    makedocs(
+        sitename = "DeformationPaths.jl",
+        pages = [
+            "Home" => "index.md",
+            "Usage Guide" => "usage.md",
+            "API Reference" => [
+                "DeformationPath" => "DeformationPath.md", 
+                "ConstraintSystem" => "ConstraintSystems.md", 
+                "Visualization" => "Visualization.md",
+                "Auxiliary Methods" => "Auxiliary.md",
+            ]    
+        ],
+        format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true", sidebar_sitename = true, assets = ["assets/custom.css"]),
+        authors = "Matthias Himmelmann",
+        modules = [DeformationPaths.DeformationPaths],
+        remotes = nothing
+    )
+end
 
 deploydocs(
     repo = "github.com/matthiashimmelmann/DeformationPaths.jl",
