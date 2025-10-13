162fb28aa374737523b994a4b3f217a7a747cd6c
matthiashimmelmann <matthias.himmelmann@outlook.de>
2025-10-13 16:19:47 +0200

    up

--- src/DeformationPaths.jl
+++ src/DeformationPaths.jl
@@ -484,7 +484,7 @@ function DeformationPath_EdgeContraction(F::Polytope, edge_for_contraction::Unio
         try
             cur_point = motion_samples[end] + 0.05*(rand(Float64,length(motion_samples[end]))-[0.5 for i in eachindex(motion_samples[end])])
             local_equations = evaluate(_G.equations, c => start_c_value - local_step_size)
-            cur_point = newton_correct(local_equations, _G.variables, _G.jacobian, cur_point; tol=tol, time_penalty=6)
+            cur_point = newton_correct(local_equations, _G.variables, _G.jacobian, cur_point; tol=tol, time_penalty=4)
             push!(motion_samples, cur_point)
             break
         catch err
